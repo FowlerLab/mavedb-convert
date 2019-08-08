e16b225133a9d65237e70dc38bc0ed62ade7b089
daniaki <danielce90@gmail.com>
2019-08-08 14:50:08 +1000

    added `install_requires`

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup
 
 setup(
     name="mavedbconvert",
-    version="0.5.0-alpha",
+    version="0.6.0-alpha",
     packages=["mavedbconvert", "mavedbconvert.tests"],
     url="https://github.com/FowlerLab/mavedb-convert",
     license="AGPLv3",
     author="Daniel Esposito",
     author_email="esposito.d@wehi.edu.au",
-    description="A command line tool for converting alternate "
-    "file formats into a MaveDB compliant format.",
+    description=(
+        "A command line tool for converting alternate "
+        "file formats into a MaveDB compliant format."
+    ),
+    install_requires=open("requirements/install.txt", "rt").read().split("\n"),
     entry_points={
         "console_scripts": ["mavedb-convert=mavedbconvert.main:main"]
     },
